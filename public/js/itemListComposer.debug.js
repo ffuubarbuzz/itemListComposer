214fc1d23b11bb440e93ef8b82d4e9839023f752
Paul Kozlovitch <Paul.Kozlovitch@gmail.com>
2013-07-29 14:59:08 +0300

    *.debug deleted again

--- public/js/itemListComposer.debug.js
+++ public/js/itemListComposer.debug.js
@@ -1,21 +1,23 @@
 D:\testroom\itemListComposer\public\js\itemListComposer.js: line 44, col 14, Missing semicolon.
 D:\testroom\itemListComposer\public\js\itemListComposer.js: line 61, col 14, Missing semicolon.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 91, col 44, Use '!==' to compare with 'undefined'.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 104, col 71, Missing semicolon.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 108, col 111, Missing semicolon.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 115, col 6, Mixed spaces and tabs.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 86, col 44, Use '!==' to compare with 'undefined'.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 101, col 111, Missing semicolon.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 108, col 6, Mixed spaces and tabs.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 110, col 6, Mixed spaces and tabs.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 111, col 6, Mixed spaces and tabs.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 112, col 6, Mixed spaces and tabs.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 113, col 6, Mixed spaces and tabs.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 114, col 6, Mixed spaces and tabs.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 116, col 6, Mixed spaces and tabs.
 D:\testroom\itemListComposer\public\js\itemListComposer.js: line 117, col 6, Mixed spaces and tabs.
 D:\testroom\itemListComposer\public\js\itemListComposer.js: line 118, col 6, Mixed spaces and tabs.
 D:\testroom\itemListComposer\public\js\itemListComposer.js: line 119, col 6, Mixed spaces and tabs.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 120, col 6, Mixed spaces and tabs.
 D:\testroom\itemListComposer\public\js\itemListComposer.js: line 121, col 6, Mixed spaces and tabs.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 123, col 6, Mixed spaces and tabs.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 124, col 6, Mixed spaces and tabs.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 125, col 6, Mixed spaces and tabs.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 126, col 6, Mixed spaces and tabs.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 128, col 6, Mixed spaces and tabs.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 129, col 6, Mixed spaces and tabs.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 166, col 47, Missing semicolon.
-D:\testroom\itemListComposer\public\js\itemListComposer.js: line 253, col 31, 'e' is already defined.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 122, col 6, Mixed spaces and tabs.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 152, col 34, 'acceptor' is already defined.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 153, col 42, Missing semicolon.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 155, col 42, Missing semicolon.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 163, col 47, Missing semicolon.
+D:\testroom\itemListComposer\public\js\itemListComposer.js: line 250, col 31, 'e' is already defined.
 
-19 errors
+21 errors
